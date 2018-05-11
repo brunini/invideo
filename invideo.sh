2a58091034477ed9a58b9296a82fbaf425d7d246
Bruno Brunini <xbrunini@gmail.com>
2018-05-10 21:12:25 -0400

    Some cleanings and added function to generate and prev list

--- invideo.sh
+++ invideo.sh
@@ -7,14 +7,14 @@ HASH_LIST=$DEST_FOLDER/.hash_list
 
 echo "BRUNINI invideo v0.1 alpha"
 
-if [ $ARG = '-m' ] || [ $ARG = '-f' ]; then
-    echo 'Starting...'
+if [[ $ARG = "-m" ]] || [[ $ARG = "-f" ]] || [[ $ARG = "-h" ]] || [[ $ARG = "-c" ]]; then
+    echo ""
 else
-    echo 'You must specify an first argument, use -h to see the documentation.'
+    echo "You must specify an valid argument, use -h to see the documentation."
     exit 0
 fi
 
-if [ $ARG = '-m' ]; then
+if [ $ARG = "-m" ]; then
     for LINE in $(df | tr -s " " | cut -d " " -f1,6)
     do
         if [ $INPUT_TARGET = $LINE ]; then
@@ -29,29 +29,48 @@ if [ $ARG = '-m' ]; then
     echo "$INPUT_TARGET is now mounted in $TARGET."
 fi
 
-if [ $ARG = '-f' ]; then
+if [ $ARG = "-f" ]; then
     TARGET=$INPUT_TARGET
 fi
 
-if [ $ARG = '-h' ]; then
-    echo '
-    Usage: invideo [OPTION] [TARGET] [DESTINATION_FOLDER]
+if [ $ARG = "-c" ]; then
+    TARGET=$INPUT_TARGET
+    HASH_LIST=$INPUT_TARGET/.hash_list
+fi
+
+if [ $ARG = "-h" ]; then
+    echo "
+    Usage: invideo [OPTION] [TARGET] [DESTINATION_FOLDER] [HASH_LIST]
     Copy videos files from memory into folders if the file hash not exists on incremental list.
 
     Mandatory arguments are.
     -f                  Specify an already mounted folder as target to searching video files and copy.
-    -m                  Specify device to be temporary mounted  and user as target to searching video files and copy.'
+    -m                  Specify device to be temporary mounted  and user as target to searching video files and copy.
+    -c                  Generate hash list of previous copied files and don't copy anything."
     exit 0
 fi
 
+if [ -z "$4" ]; then
+    echo "Hash list is not specified."
+else
+    HASH_LIST=$4
+fi
 
-echo "Searching for videos in $TARGET."
+if [ -f $HASH_LIST ]; then
+   echo "Using hash list in $HASH_LIST."
+else
+   echo "Creating hash list in $HASH_LIST."
+   touch $HASH_LIST
+fi
+
+echo ""
+echo "Searching for files in $TARGET."
 find $TARGET -type f -name "*.MTS" -o -name "*.MPG" -o -name "*.MOV"
 for FILE_PATH in $(find $TARGET -type f -name "*.MTS" -o -name "*.MPG" -o -name "*.MOV" -o -name "*.JPG")
 do
     EXISTS=0
     echo ''
-    echo "Getting file hash of $FILE_PATH"
+    echo "Calculating partial file hash of $FILE_PATH"
     FILE_HASH=$(cat $FILE_PATH | head -n 30720 | sha512sum  | cut -d " " -f1)
     SH=${FILE_HASH:0:15}
     echo "Verify if $SH exists on hash list"
@@ -63,19 +82,23 @@ do
         fi
     done
     if [ $EXISTS -eq 1 ]; then
-        echo $SH "exists in list, avoiding copy."
+        echo $SH "exists in hash list, passing."
     else
-        echo $SH "not exists in list, copying."
-        FILENAME=$(basename -- "$FILE_PATH")
-        EXTENSION="${FILENAME##*.}"
-        FILENAME="${FILENAME%.*}"
-        pv $FILE_PATH > $DEST_FOLDER/$FILENAME.$SH.$EXTENSION
+        if [ $ARG = "-c" ]; then
+            echo "Writing full hash to the list."
+        else
+            echo $SH "not exists in hash list, copying."
+            FILENAME=$(basename -- "$FILE_PATH")
+            EXTENSION="${FILENAME##*.}"
+            FILENAME="${FILENAME%.*}"
+            pv $FILE_PATH > $DEST_FOLDER/$FILENAME.$SH.$EXTENSION
+        fi
         echo $FILE_HASH >> $HASH_LIST 
     fi
 done
 
 
-if [ $TARGET = $TMP ]; then
+if [[ $TARGET = $TMP ]]; then
     umount $TMP
     rm -rf $TMP
 echo "Temporary mounting point is now unmonted and deleted."
